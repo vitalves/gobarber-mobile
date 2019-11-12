ebc6ae98b2f44d0a513f104977149e607493055e
vitalves <vitalvesilva@gmail.com>
2019-11-12 17:10:14 -0300

    Input & Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 // import { Container } from './styles';
 
@@ -10,6 +12,14 @@ export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
