c7f3fa3d236251145d05190dcd1debd93f643918
vitalves <vitalvesilva@gmail.com>
2019-11-19 21:46:53 -0300

    Rotas de agendamento

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
+import React from 'react'; // jsx no Icon
+
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 // Login e cadastro:
 import SignIn from '~/pages/SignIn';
@@ -9,6 +14,10 @@ import SignUp from '~/pages/SignUp';
 // logado:
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
+
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
 
 /*
 export default createAppContainer(
@@ -32,7 +41,44 @@ export default (signedIn = false) =>
         // Rotas para usuário logado:
         App: createBottomTabNavigator(
           {
+            /* Dashboard: */
             Dashboard,
+
+            /* Agendar */
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  // padroes para as paginas
+                  headerLayoutPreset: 'center',
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#FFF',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                },
+              ),
+              // configuracoes do Agendar
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                ),
+              },
+            },
+
+            /* Profile: */
             Profile,
           },
           // configuracoes da createBottomTabNavigator:
