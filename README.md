36f069109b3eb71d94131c27c17c5960a29798f3
vitalves <vitalvesilva@gmail.com>
2019-11-21 16:55:00 -0300

    Confirmando agendamento

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 **Alterar o IP da url do Avatar em _src/components/Appointment/index.js_**
 **Alterar o IP da url do Avatar em _src/pages/New/SelectProvider/index.js_**
+**Alterar o IP da url do Avatar em _src/pages/New/Confirm/index.js_**
 
 _data.provider.avatar.url_
--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,75 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity, Alert } from 'react-native';
 
-// import { Container } from './styles';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-export default function Confirm() {
-  return <View />;
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
+
+import Background from '~/components/Background';
+
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+export default function Confirm({ navigation }) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time],
+  );
+
+  async function handleAddAppointment() {
+    const response = await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    if (response.data) {
+      Alert.alert(
+        'Sucesso',
+        `Agendamento realizado com '${provider.name}' para ${dateFormatted}`,
+      );
+
+      navigation.navigate('Dashboard');
+    }
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.Avatar
+              ? // provider.avatar.url
+                `http://10.0.3.2:3333/files/${provider.avatar.path}`
+              : `https://api.adorable.io/avatar/120/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirmar agendamento',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={25} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
